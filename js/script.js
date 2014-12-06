dde276e7d9d6b3eeac6189bc692e443f6c1d097e
kellyjovan <jovanjkelly@yahoo.com>
2014-12-05 21:36:38 -0500

    added school website files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  // $('.back_to_top').click(function(){
+  //   alert("Clicked");
+  // })
+  var menu = $('.nav');
+  var origOffsetY = menu.offset().top;
+  $('.back_to_top').hide();
+  function scroll() {
+    if ($(window).scrollTop() >= origOffsetY) {
+      $('.back_to_top').fadeIn(900);
+    } else {
+      $('.back_to_top').fadeOut(900);
+    }
+  }
+  document.onscroll = scroll;
+
+
+  $('.anchor').click(function(){
+      $('html, body').animate({
+          scrollTop: $( $(this).attr('href') ).offset().top
+      }, 500);
+      return false;
+    });
+
+  $('.carousel').carousel();
+})
