1724d101283719410721c98c3a9289fa3db05dc7
kellyjovan <jovanjkelly@yahoo.com>
2015-01-09 09:07:44 -0500

    linked department pages

--- static/js/script2.js
+++ static/js/script2.js
@@ -1,19 +1,10 @@
 $(document).ready(function(){
-
-  // $('.back_to_top').click(function(){
-  //   alert("Clicked");
-  // })
-  var menu = $('#nav');
-  var origOffsetY = menu.offset().top;
+  var origOffsetY = 1;
   $('.back_to_top').hide();
   function scroll() {
     if ($(window).scrollTop() >= origOffsetY) {
       $('.back_to_top').fadeIn(500);
-      $('#nav').removeClass('nav_abs');
-      $('#nav').addClass('nav_fixed');
     } else {
-      $('#nav').addClass('nav_abs');
-      $('#nav').removeClass('nav_fixed');
       $('.back_to_top').fadeOut(500);
     }
   }
@@ -25,6 +16,4 @@ $(document).ready(function(){
       }, 500);
       return false;
     });
-
-  $('.carousel').carousel();
-})
+});
